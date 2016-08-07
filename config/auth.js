2221e09378a1e740a638f14cc2b2548598040750
Theodore P <evil69669@gmail.com>
2016-08-07 22:32:15 +0300

    :D

--- config/auth.js
+++ config/auth.js
@@ -4,9 +4,9 @@
 module.exports = {
 
         'googleAuth' : {
-        'clientID'      : '603682516969-vgfdciapqpc0mjvv49enjhngsv1rr9gu.apps.googleusercontent.com',
-        'clientSecret'  : 'QRtuE6IYIG7MC-FFqEpNi1_U',
-        'callbackURL'   : 'https://pinterest123.herokuapp.com/auth/google/callback'
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : ''
     }
 
 };
